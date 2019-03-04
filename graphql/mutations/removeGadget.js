9721839039bdeffb5d075350584f6a0c04a219ab
Isac Moura <contatoisacmoura@outlook.com>
2019-03-04 03:04:03 -0300

    Fixing erros

--- graphql/mutations/removeGadget.js
+++ graphql/mutations/removeGadget.js
@@ -1,18 +1,16 @@
 const { GraphQLObjectType, GraphQLString } = require('graphql');
-const gadgetGraphQLType = require('../gadgetType');
-const Gadget = require('../../models/gadgets');
+const gadgetGraphQLType =  require('./../gadgetType');
+const Gadget = require('./../../models/gadget');
 
 module.exports = {
-    removeGadget: {
-        type: gadgetGraphQLType,
-        args: {
-            id: { type:GraphQLString }
-        },
-        resolve(parent, args) {
-            return Gadget.findOneAndDelete(args.id).exec()
-                .then(gadget => gadget.remove())
-                .then(deletedGadget => deletedGadget)
-                .catch(err => console.log(err))
-        }
-    }
-}
+  type: gadgetGraphQLType,
+  args: {
+    id: { type: GraphQLString }
+  },
+  resolve(parent, args) {
+    return Gadget.findOneAndDelete(args.id).exec()
+      .then(gadget => gadget.remove())
+      .then(deletedGadget => deletedGadget)
+      .catch(err => console.log(err))
+  }
+};
--- graphql/mutations/updateGadget.js
+++ graphql/mutations/updateGadget.js
@@ -1,29 +1,28 @@
 const { GraphQLObjectType, GraphQLString } = require('graphql');
-const gadgetGraphQLType = require('../gadgetType');
-const Gadget = require('../../models/gadgets');
+const gadgetGraphQLType =  require('./../gadgetType');
+const Gadget = require('./../../models/gadget');
 
 module.exports = {
-    updateGadget: {
-        type: gadgetGraphQLType,
-        args: {
-            id: { type: GraphQLString },
-            name: { type: GraphQLString },
-            release_date: { type: GraphQLString },
-            by_company: { type: GraphQLString },
-            price: { type: GraphQLString }
-        },
-        resolve(patent, args) {
-            return Gadget.findById(args.id)
-                .then(gadget => {
-                    gadget.name = args.name,
-                    gadget.release_date = args.release_date,
-                    gadget.by_company = args.by_company,
-                    gadget.price = args.price
-    
-                    return gadget.save()
-                })
-                .then(updatedGadget => updatedGadget)
-                .catch(err => console.log(err))
-        }
-    }
-}
+  type: gadgetGraphQLType,
+  args: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    release_date: { type: GraphQLString },
+    by_company: { type: GraphQLString },
+    price: { type: GraphQLString }
+  },
+  resolve(parent, args) {
+    return Gadget.findById(args.id)
+      .then(gadget => {
+        gadget.name = args.name
+        gadget.release_date = args.release_date,
+        gadget.by_company = args.by_company,
+        gadget.price = args.price
+
+        return gadget.save()
+
+      })
+      .then(updatedGadget => updatedGadget)
+      .catch(err => console.log(err))
+  }
+};
