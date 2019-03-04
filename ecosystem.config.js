fcf83cc74b89d12166edde314196b35eedfa0981
Isac Moura <contatoisacmoura@outlook.com>
2019-03-04 04:18:57 -0300

    Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
+    name: 'koa-graphql-mongo-api',
+    script: 'server.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
@@ -15,16 +15,5 @@ module.exports = {
     env_production: {
       NODE_ENV: 'production'
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
