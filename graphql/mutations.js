7c0696d8caf688c5cca78d65ab1e9a2826eece23
Isac Moura <contatoisacmoura@outlook.com>
2019-03-04 02:16:42 -0300

    Update and delete mutations

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -22,6 +22,41 @@ const Mutation = new GraphQLObjectType({
                 })
                 return newGadget.save();
             }
+        },
+        updateGadget: {
+            type: gadgetGraphQLType,
+            args: {
+                id: { type: GraphQLString },
+                name: { type: GraphQLString },
+                release_date: { type: GraphQLString },
+                by_company: { type: GraphQLString },
+                price: { type: GraphQLString }
+            },
+            resolve(patent, args) {
+                return Gadget.findById(args.id)
+                    .then(gadget => {
+                        gadget.name = args.name,
+                        gadget.release_date = args.release_date,
+                        gadget.by_company = args.by_company,
+                        gadget.price = args.price
+
+                        return gadget.save()
+                    })
+                    .then(updatedGadget => updatedGadget)
+                    .catch(err => console.log(err))
+            }
+        },
+        removeGadget: {
+            type: gadgetGraphQLType,
+            args: {
+                id: { type:GraphQLString }
+            },
+            resolve(parent, args) {
+                return Gadget.findOneAndDelete(args.id).exec()
+                    .then(gadget => gadget.remove())
+                    .then(deletedGadget => deletedGadget)
+                    .catch(err => console.log(err))
+            }
         }
     }
 })
